c23a101b4479bb5f61dc000ee4937ac6503b4072
Olga Krutchenko <krutchenko.olga@gmai.com>
2022-07-04 11:31:57 +0300

    Выполнена четвертая домашняя работа.

--- src/main/java/homework4/Main.java
+++ src/main/java/homework4/Main.java
@@ -10,7 +10,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println(Arrays.equals(method1(), method2()));
-        System.out.println(method1() == method2());
 
     }
 
@@ -28,9 +27,9 @@ public class Main {
     public static float[] method2() {
         float[] arr = new float[size];
         Arrays.fill(arr, 1);
+        long a = System.currentTimeMillis();
         float[] a1 = new float[h];
         float[] a2 = new float[h];
-        long a = System.currentTimeMillis();
         System.arraycopy(arr, 0, a1, 0, h);
         System.arraycopy(arr, h, a2, 0, h);
 
